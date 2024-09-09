27abb0ffa3052f1b528d0652f1fec0efafd6fbe7
clinton-slice <ccnkemdilim@gmail.com>
2024-09-09 11:32:20 -0400

    refactor: Update Stores page component layout

--- client/src/pages/Stores.tsx
+++ client/src/pages/Stores.tsx
@@ -27,17 +27,13 @@ const Stores = () => {
     );
   }
 
-  console.log(stores);
-
   return (
-    <main className="lg:pl-72">
-      <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
-        <h1 className="text-base font-semibold leading-6 text-gray-900 mb-4">
-          Stores
-        </h1>
-        <StoreList stores={stores} />
-      </div>
-    </main>
+    <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
+      <h1 className="text-base font-semibold leading-6 text-gray-900 mb-4">
+        Stores
+      </h1>
+      <StoreList stores={stores} />
+    </div>
   );
 };
 
